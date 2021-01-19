89c1e62f2b650cc2fa1b47f90e87996117ddbb3e
Ayamohamedaya <ayamohammedh605@gmail.com>
2021-01-19 17:54:40 +0200

    no message

---
+++ Assets/Scripts/PlayerCamra.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class PlayerCamra : MonoBehaviour
+{
+    GameObject player;
+    public void LookAtPlayer()
+    {
+        player = GameObject.FindWithTag("Player"); 
+        GetComponent<CinemachineVirtualCamera>().Follow=player.transform;
+    }
+}
